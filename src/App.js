b407cb8cd0a3b772c13caf395a32a0864bbf27b8
Isma1lov-nob <115091585+Isma1lov-nob@users.noreply.github.com>
2023-02-08 10:04:54 +0600

    hw-7

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from "react";
-import ImageSlider from "./components/ImageSlider";
-import { SliderData } from "./components/SliderData";
-import "./App.css"
 
-const App = ()=>{
-return(
-  <div>
-    <ImageSlider slides={SliderData}></ImageSlider>
-  </div>
-)
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    
+    this.state ={
+      title: "текущее время",
+      date: new Date (),
+      counter : 0
+    }
+    
+    setInterval (()=>{
+      this.setState((state , props)=>({
+        date : new Date(),
+        counter :  state.counter + 1
+      }))
+    }, 1000)
+  }
+  render(){
+    return(
+      <div>
+        <p>{this.state.title}</p>
+        <p>{this.state.counter}</p>
+        <p>{this.state.date.toLocaleTimeString()}</p>
+      </div>
+    )
+  }
+
+
 }
+
 export default App
