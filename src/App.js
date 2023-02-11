16790e29089de13633e8fe861f913c8c66aebf5f
Isma1lov-nob <115091585+Isma1lov-nob@users.noreply.github.com>
2023-02-11 12:07:48 +0600

    hw-8

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import React from "react";
 
-class App extends React.Component{
-  constructor(props){
-    super(props)
-    
-    this.state ={
-      title: "текущее время",
-      date: new Date (),
-      counter : 0
-    }
-    
-    setInterval (()=>{
-      this.setState((state , props)=>({
-        date : new Date(),
-        counter :  state.counter + 1
-      }))
-    }, 1000)
-  }
-  render(){
-    return(
-      <div>
-        <p>{this.state.title}</p>
-        <p>{this.state.counter}</p>
-        <p>{this.state.date.toLocaleTimeString()}</p>
-      </div>
-    )
-  }
-
-
+import React from 'react'
+import Contact from './Contact'
+function App() {
+  return (
+    <div>
+      <Contact  name = "baiel">
+        <h1>hello</h1>
+        <h1>ismailov</h1>
+      </Contact>
+    </div>
+  )
 }
 
 export default App
+
+
